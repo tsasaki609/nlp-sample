fbb4f45f2a4400e04ab06a3e36b319b77c83106d
Takashi Sasaki <tsasaki609@gmail.com>
2023-04-09 13:54:47 +0900

    commenting

--- fine-tunening-word2vec.py
+++ fine-tunening-word2vec.py
@@ -22,8 +22,14 @@ with open(CUSTOM_DATASET_PATH, encoding='utf-8') as f:
 for sentence in sentences:
     doc = nlp(sentence)
     for token in doc:
+        # TODO このコードは追加のデータセットに既知の固有名詞入っている前提でトークン分割してエンティティタイプを絞って追加している
+        # 既に固有名詞のみで構成される場合はトークン分割など不要であるのでそのまま投入すれば良さそう
+        # その場合、既に含まれる固有名詞は除外する
+        # ex. if word in model.wv.vocab and model.wv.vocab[word].count > 1:
+
         if token.ent_type_ == "LOC" or token.ent_type_ == "ORG":
             model.wv.vocab[token.text] = gensim.models.word2vec.Vocab(count=1, index=len(model.wv.vocab))
+    # TODO 毎度毎度学習するのは非効率なので工夫の余地がある
     model.train([doc], total_examples=1, epochs=1)
 
 # Fine-tuning後のモデルを保存
